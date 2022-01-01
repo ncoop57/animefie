1c5de3113abcd869b92bf8f6c55ac1513f3281dc
ncoop57 <nacooper01@email.wm.edu>
2022-01-01 10:53:30 -0500

    Add support for different models/effects to images

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -8,22 +8,45 @@ import 'package:gallery_saver/gallery_saver.dart';
 import 'package:http/http.dart' as http;
 import 'package:image/image.dart' as img;
 import 'package:flutter/services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:native_device_orientation/native_device_orientation.dart';
 
 import 'gallery_screen.dart';
 
-Future<Uint8List> animefyImage(String imageBase64) async {
+// Supported models
+Map<String, List<String>> urls = {
+  'ArcaneGAN': [
+    'https://hf.space/gradioiframe/akhaliq/ArcaneGAN/api/predict',
+    'version 0.4'
+  ],
+  'AnimeGANv2': [
+    'https://hf.space/gradioiframe/akhaliq/AnimeGANv2/api/predict',
+    'version 2 (\ud83d\udd3a stylization, \ud83d\udd3b robustness)'
+  ],
+  'JoJoGAN': [
+    'https://hf.space/gradioiframe/akhaliq/JoJoGAN/api/predict',
+    'Disney'
+  ],
+  'GFP-GAN': ['https://hf.space/gradioiframe/akhaliq/GFPGAN/api/predict'],
+  'Apocalyptify': [
+    'https://hf.space/gradioiframe/Norod78/Apocalyptify/+/api/predict'
+  ],
+};
+
+Future<Uint8List> animefyImage(String imageBase64, String model) async {
+  var data = [imageBase64];
+  // Check if the model requires additional parameters
+  if (urls[model]!.length > 1) {
+    data.add(urls[model]![1]);
+  }
   // Animefy the given image by requesting the gradio API of AnimeGANv2
   final response = await http.post(
-    Uri.parse('https://hf.space/gradioiframe/akhaliq/AnimeGANv2/api/predict'),
+    Uri.parse(urls[model]![0]),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, List<String>>{
-      'data': [
-        imageBase64,
-        'version 2 (\ud83d\udd3a stylization, \ud83d\udd3b robustness)'
-      ]
+      'data': data,
     }),
   );
 
@@ -31,7 +54,8 @@ Future<Uint8List> animefyImage(String imageBase64) async {
     // If the server did return a 200 CREATED response,
     // then decode the image and return it.
     final imageData = jsonDecode(response.body)["data"][0]
-        .replaceAll('data:image/png;base64,', '');
+        .replaceAll('data:image/png;base64,', '')
+        .replaceAll('data:image/jpeg;base64,', ''); // Some models return jpeg
     return base64Decode(imageData);
   } else {
     // If the server did not return a 200 OKAY response,
@@ -61,6 +85,9 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  String dropdownValue = 'ArcaneGAN';
+  bool isLoading = false;
+
   @override
   void initState() {
     SystemChrome.setPreferredOrientations([
@@ -152,6 +179,30 @@ class _CameraScreenState extends State<CameraScreen> {
             },
           ),
           const Spacer(),
+          Center(
+            child: Theme(
+              data: Theme.of(context).copyWith(
+                canvasColor: Colors.black,
+              ),
+              child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items:
+                      urls.keys.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList()),
+            ),
+          ),
+          const Spacer(),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Row(
@@ -177,41 +228,65 @@ class _CameraScreenState extends State<CameraScreen> {
                   icon: const Icon(Icons.switch_camera_rounded,
                       color: Colors.white),
                 ),
-                GestureDetector(
-                  onTap: () async {
-                    await _initializeControllerFuture;
+                AnimatedSwitcher(
+                  // From: https://stackoverflow.com/a/62779504
+                  duration: const Duration(milliseconds: 300),
+                  child: isLoading
+                      ? const SpinKitSpinningLines(
+                          key: Key("loading"),
+                          color: Colors.white,
+                          size: 50.0,
+                        )
+                      : GestureDetector(
+                          key: const Key("normal"),
+                          onTap: () async {
+                            _toggle();
+                            await _initializeControllerFuture;
 
-                    // Take photo and fix any rotation issues
-                    final xFile = await _controller.takePicture();
-                    final imgFile = File(xFile.path);
-                    await applyRotationFix(imgFile);
+                            // Take photo and fix any rotation issues
+                            final xFile = await _controller.takePicture();
+                            final imgFile = File(xFile.path);
+                            await applyRotationFix(imgFile);
 
-                    // Animefy the image
-                    final imgBytes = imgFile.readAsBytesSync();
-                    String base64Image =
-                        "data:image/png;base64," + base64Encode(imgBytes);
-                    final animeBytes = await animefyImage(base64Image);
+                            // Animefy the image
+                            final imgBytes = imgFile.readAsBytesSync();
+                            String base64Image = "data:image/png;base64," +
+                                base64Encode(imgBytes);
 
-                    // Save the image
-                    imgFile.writeAsBytesSync(animeBytes);
-                    GallerySaver.saveImage(imgFile.path);
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text('Image saved to gallery'),
-                      duration: Duration(seconds: 2),
-                    ));
+                            try {
+                              final animeBytes = await animefyImage(
+                                  base64Image, dropdownValue);
 
-                    setState(() {
-                      capturedImages.add(imgFile);
-                    });
-                  },
-                  child: Container(
-                    height: 60,
-                    width: 60,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                  ),
+                              // Save the image
+                              imgFile.writeAsBytesSync(animeBytes);
+                              GallerySaver.saveImage(imgFile.path);
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content: Text('Image saved to gallery'),
+                                duration: Duration(seconds: 2),
+                              ));
+
+                              setState(() {
+                                capturedImages.add(imgFile);
+                              });
+                            } catch (e) {
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content: Text('An Error Occurred'),
+                                duration: Duration(seconds: 2),
+                              ));
+                            }
+                            _toggle();
+                          },
+                          child: Container(
+                            height: 60,
+                            width: 60,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
                 ),
                 GestureDetector(
                   onTap: () {
@@ -242,5 +317,11 @@ class _CameraScreenState extends State<CameraScreen> {
         ],
       ),
     );
+  }
+
+  _toggle() {
+    setState(() {
+      isLoading = !isLoading;
+    });
   }
 }
